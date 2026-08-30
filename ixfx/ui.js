6b430f39832babb4aafc28b873fcdc0559c5a6a1
CH <clint@thestaticvoid.net>
2026-08-31 01:16:26 +0200

    ixfx

--- ixfx/ui.js
+++ ixfx/ui.js
@@ -1,10 +1,10 @@
 import { n as __exportAll } from "./chunk-CaR5F9JI.js";
-import { C as beforeMatch, D as stringSegmentsWholeToEnd, O as stringSegmentsWholeToFirst, S as afterMatch, nt as findBySomeKey, q as getPathsAndData } from "./src-D552aW4b.js";
-import { v as QueueMutable } from "./src-CkNW04JY.js";
-import { W as resolveEl } from "./src-CvrfRWyP.js";
-import "./src-D8pW0Dyk.js";
-import { _ as fromCss, g as fromCss$1, h as toCssColour, m as toColour, v as toCssString } from "./src-rbv0r-Un.js";
-import { $ as transform, X as object, _ as event, at as initStream, bt as messageIsSignal, h as observable, lt as hasLast, q as debounce, v as eventTrigger, vt as messageHasValue } from "./src-B_TDHMf3.js";
+import { C as beforeMatch, D as stringSegmentsWholeToEnd, O as stringSegmentsWholeToFirst, S as afterMatch, Y as getPathsAndData, it as findBySomeKey } from "./src-CHZXopuG.js";
+import { v as QueueMutable } from "./src-DnUdDLFW.js";
+import { W as resolveEl } from "./src-BpBV9w4H.js";
+import "./src-BVT615DA.js";
+import { _ as fromCss, g as fromCss$1, h as toCssColour, m as toColour, v as toCssString } from "./src-DoTq0GTE.js";
+import { $ as transform, X as object, _ as event, at as initStream, bt as messageIsSignal, h as observable, lt as hasLast, q as debounce, v as eventTrigger, vt as messageHasValue } from "./src-DyvEViDQ.js";
 
 //#region ../packages/ui/src/rx/browser-resize.ts
 /**
