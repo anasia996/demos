24dd1a27ed91dea7a3e68e6da67ff1c2b9adee23
anasia996 <anastasiaa.titovaa@gmail.com>
2026-09-07 10:01:51 +0200

    simplify

--- barebones copy/script.js
+++ barebones copy/script.js
@@ -1,23 +1,22 @@
 import * as Random from '@ixfx/random.js';
 
-const squareEl = document.getElementById("square");
+const square = document.getElementById("square");
 
-/**
+/*/**
  * Define the type for 'State'
  * @typedef {Readonly<{
  * }>} State
  */
 
-/** @type State */
-let state = Object.freeze({
+//** @type State */
+/*let state = Object.freeze({
 });
-
+*/
 let hue = 270;
-let direction = 1; // 1 = increasing, -1 = decreasing
+let direction = 1; 
 
 function changeColor() {
   hue += 20 * direction;
-
   if (hue >= 360) {
     hue = 360;
     direction = -1;
@@ -25,25 +24,24 @@ function changeColor() {
     hue = 180;
     direction = 1;
   }
-
-  squareEl.style.backgroundColor = `hsl(${hue}, 90%, 96%)`;
+  square.style.backgroundColor = `hsl(${hue}, 90%, 96%)`;
 }
 
 function setup() {
   document.addEventListener("keyup", changeColor);
 }
-
+/*
 /**
  * Saves the state
  * @param {Partial<State>} s 
  * @returns 
  */
 
-function saveState(s) {
+/*function saveState(s) {
   state = Object.freeze({
     ...state,
     ...s
   });
   return state;
-}
+}*/
 setup();
